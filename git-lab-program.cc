d511cbe8ec394593a3d96ca0b95afd5366ba57fc
Connor Sperdute <cs357317@ohio.edu>
2021-01-27 17:19:57 -0500

    First get program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Connor Sperdute
+ *        Date: 1/27/2021
+ * Description: First code going to Git
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
